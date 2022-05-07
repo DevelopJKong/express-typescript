b2a928b5e0c6c27ad121be0a3a650be85ecaf594
DevelopJKong <ybaram40@gmail.com>
2022-05-07 17:20:10 +0900

    user table 만들기 성공 & git 연구하기

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,9 @@
-import { useEffect, useState } from "react";
 import Router from "./Router";
 
 function App() {
-  const [info, setInfo] = useState("");
-  const callApi = async () => {
-    const data = await (await fetch(`/api`)).json();
-    setInfo(data.hello);
-  };
-
-  useEffect(() => {
-    callApi();
-  }, []);
-
   return (
     <>
       <Router />
-      {info}
     </>
   );
 }
